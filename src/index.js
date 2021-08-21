719f4dc9e6788f34fc6ab32a614d7728a853d015
f <urka94@bk.ru>
2021-08-22 01:52:17 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,50 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrStr= expr.split('');
+    let morseArr=[];
+    let morseBinary=[];
+    for (let i = 0; i < arrStr.length; i++) {
+      for (let key in MORSE_TABLE) {
+        if (arrStr[i] == MORSE_TABLE[key]) {
+          morseArr.push(key)
+        }
+      }
+    }
+  for (let i = 0; i < morseArr.length; i++) {
+    let morseSplit = morseArr[i].split('');
+    let arrNum = [];
+    let arrSplit =[];
+        if (morseArr[i].includes("*")) {
+          arrNum.push("**********")
+        }
+    morseSplit.forEach(element => {
+      if (element == '-') {
+        arrNum.push("11");
+      }
+      if(element == '.'){
+      arrNum.push("10");
+      }
+    });
+    arrNum.forEach(elem =>{
+      let num = elem.split('');
+      num.forEach(num => {
+         arrSplit.push(num);
+      })
+    });
+    for (let i = 0; i < 10; i++) {
+      if (arrSplit.length < 10) {
+        arrSplit.unshift('0');
+      }
+    }
+    morseBinary.push(arrSplit.join(''));
+  } 
+  console.log(arrStr);
+  return morseBinary.join('');
 }
 
 module.exports = {
